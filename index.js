155bd7a19330dc98c3cbbc2f331c6a2101bc81e9
jeffritoo <jeffryvdavid@gmail.com>
2021-04-27 17:47:50 -0700

    final edit

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: false })); // <--- middleware configuration
 
 
